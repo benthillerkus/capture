c88566b07785c4b96ac7956e04b269e7b599d986
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-01-11 16:36:32 +0100

    chore: change recording settings

--- src/camera.rs
+++ src/camera.rs
@@ -88,7 +88,7 @@ impl CameraActor {
                         .build()
                         .unwrap();
 
-                    caps = gstreamer::Caps::from_str("video/x-raw(memory:NVMM),width=(int)1280,height=(int)720,format=(string)NV12,framerate=(fraction)30/1").unwrap();
+                    caps = gstreamer::Caps::from_str("video/x-raw(memory:NVMM),width=(int)1920,height=(int)1080,format=(string)RGB,framerate=(fraction)60/1").unwrap();
 
                     left_enc = ElementFactory::make("nvjpegenc")
                         .property_from_str("quality", "95")
